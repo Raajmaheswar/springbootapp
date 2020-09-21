1c792c96f3b3d29b98c56fca1568c8489cc156f2
Raajmaheswar <raajmaheswar@outlook.com>
2020-09-21 09:00:15 +0530

    env and sec vars added
    env and sec vars added

--- src/main/java/com/example/springboot/controller/HelloWorldController.java
+++ src/main/java/com/example/springboot/controller/HelloWorldController.java
@@ -1,14 +1,23 @@
 package com.example.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	@Value("${TEST_ENV_PROP}")
+	String profileDetails;
 	
+	@Value("${MY_SECRET:aDefaultValue}")
+	private String s1;
+
 	@GetMapping("")
 	String hello() {
-		 return "Hello world!";
+		 
+		 
+		 return "Hello world is "+profileDetails+" with secret "+s1;
 	}
+	
 
 }
